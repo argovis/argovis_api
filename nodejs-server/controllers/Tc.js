7aeb27e0090ad3437fc6cf9f5a4b87e74010bfd4
BillMills <mills.wj@gmail.com>
2021-09-21 16:03:03 -0400

    templates for tc name / year lookup

--- nodejs-server/controllers/Tc.js
+++ nodejs-server/controllers/Tc.js
@@ -42,3 +42,13 @@ module.exports.findTCbyDateRange = function findTCbyDateRange (req, res, next, s
       utils.writeJson(res, response);
     });
 };
+
+module.exports.findTCbyNameYear = function findTCbyNameYear (req, res, next) {
+  Tc.findTCbyNameYear()
+    .then(function (response) {
+      utils.writeJson(res, response);
+    })
+    .catch(function (response) {
+      utils.writeJson(res, response);
+    });
+};
--- nodejs-server/service/TcService.js
+++ nodejs-server/service/TcService.js
@@ -186,3 +186,58 @@ exports.findTCbyDateRange = function(startDate,endDate) {
   });
 }
 
+
+/**
+ * find a tropical cyclone by name and year
+ *
+ * returns tcSchema
+ **/
+exports.findTCbyNameYear = function() {
+  return new Promise(function(resolve, reject) {
+    var examples = {};
+    examples['application/json'] = {
+  "endDate" : "2000-01-23T04:56:07.000+00:00",
+  "year" : 6.027456183070403,
+  "num" : 0.8008281904610115,
+  "name" : "name",
+  "_id" : "_id",
+  "source" : "source",
+  "trajData" : [ {
+    "date" : "date",
+    "pres" : 7.061401241503109,
+    "geoLocation" : {
+      "coordinates" : [ [ 6.027456183070403, 6.027456183070403 ], [ 6.027456183070403, 6.027456183070403 ] ],
+      "type" : "type"
+    },
+    "season" : 9.301444243932576,
+    "lon" : 5.637376656633329,
+    "time" : 1.4658129805029452,
+    "class" : "class",
+    "lat" : 5.962133916683182,
+    "wind" : 2.3021358869347655,
+    "timestamp" : "2000-01-23T04:56:07.000+00:00"
+  }, {
+    "date" : "date",
+    "pres" : 7.061401241503109,
+    "geoLocation" : {
+      "coordinates" : [ [ 6.027456183070403, 6.027456183070403 ], [ 6.027456183070403, 6.027456183070403 ] ],
+      "type" : "type"
+    },
+    "season" : 9.301444243932576,
+    "lon" : 5.637376656633329,
+    "time" : 1.4658129805029452,
+    "class" : "class",
+    "lat" : 5.962133916683182,
+    "wind" : 2.3021358869347655,
+    "timestamp" : "2000-01-23T04:56:07.000+00:00"
+  } ],
+  "startDate" : "2000-01-23T04:56:07.000+00:00"
+};
+    if (Object.keys(examples).length > 0) {
+      resolve(examples[Object.keys(examples)[0]]);
+    } else {
+      resolve();
+    }
+  });
+}
+
