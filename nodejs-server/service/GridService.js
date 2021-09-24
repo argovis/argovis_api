a228a208af3943d564135ab3572ea5f9021039d3
billmills <you@example.com>
2021-09-24 16:48:02 -0400

    proper schema for grid parameter search

--- nodejs-server/service/GridService.js
+++ nodejs-server/service/GridService.js
@@ -26,12 +26,18 @@ exports.findGrid = function(gridName) {
  * gridName String name of the gridded product
  * presLevel BigDecimal pressure level (dbar)
  * param String tbd
- * returns GridSchema
+ * returns gridParams
  **/
 exports.findGridParam = function(gridName,presLevel,param) {
   return new Promise(function(resolve, reject) {
     var examples = {};
-    examples['application/json'] = "";
+    examples['application/json'] = {
+  "pres" : 0.8008281904610115,
+  "param" : "param",
+  "trend" : "trend",
+  "model" : "model",
+  "measurement" : "measurement"
+};
     if (Object.keys(examples).length > 0) {
       resolve(examples[Object.keys(examples)[0]]);
     } else {
