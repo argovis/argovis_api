0db3b52b690f00162a2f44fe90ba3434535c17b7
billmills <you@example.com>
2021-09-26 14:08:37 -0400

    match covar return schema to whats actually in the db

--- nodejs-server/service/CovarService.js
+++ nodejs-server/service/CovarService.js
@@ -15,25 +15,23 @@ exports.findCovar = function(lat,lon,forcastDays) {
     examples['application/json'] = {
   "features" : [ {
     "geometry" : {
-      "coordinates" : [ [ 0.8008281904610115, 0.8008281904610115 ], [ 0.8008281904610115, 0.8008281904610115 ] ],
+      "coordinates" : [ 6.027456183070403, 6.027456183070403 ],
       "type" : "type"
     },
-    "type" : "type"
+    "type" : "type",
+    "properties" : { }
   }, {
     "geometry" : {
-      "coordinates" : [ [ 0.8008281904610115, 0.8008281904610115 ], [ 0.8008281904610115, 0.8008281904610115 ] ],
+      "coordinates" : [ 6.027456183070403, 6.027456183070403 ],
       "type" : "type"
     },
-    "type" : "type"
+    "type" : "type",
+    "properties" : { }
   } ],
-  "dLat" : 6.027456183070403,
-  "dLong" : 1.4658129805029452,
-  "geoLocation" : {
-    "coordinates" : [ 6.027456183070403, 6.027456183070403 ],
-    "type" : "type"
-  },
+  "dLat" : 0.8008281904610115,
+  "dLong" : 6.027456183070403,
   "_id" : "_id",
-  "forcastDays" : 5
+  "forcastDays" : 1
 };
     if (Object.keys(examples).length > 0) {
       resolve(examples[Object.keys(examples)[0]]);
