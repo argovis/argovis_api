26af0167e17cb729fd2521430849d2982d7a78f0
billmills <you@example.com>
2021-09-26 14:29:59 -0400

    tc return schema correction

--- nodejs-server/service/TcService.js
+++ nodejs-server/service/TcService.js
@@ -78,12 +78,12 @@ exports.findStormNameList = function() {
  * tropical cyclones at a given date-time
  *
  * date Date date-time formatted string
- * returns tcSchema
+ * returns List
  **/
 exports.findTCbyDate = function(date) {
   return new Promise(function(resolve, reject) {
     var examples = {};
-    examples['application/json'] = {
+    examples['application/json'] = [ {
   "endDate" : "2000-01-23T04:56:07.000+00:00",
   "year" : 6.027456183070403,
   "num" : 0.8008281904610115,
@@ -120,7 +120,44 @@ exports.findTCbyDate = function(date) {
     "timestamp" : "2000-01-23T04:56:07.000+00:00"
   } ],
   "startDate" : "2000-01-23T04:56:07.000+00:00"
-};
+}, {
+  "endDate" : "2000-01-23T04:56:07.000+00:00",
+  "year" : 6.027456183070403,
+  "num" : 0.8008281904610115,
+  "name" : "name",
+  "_id" : "_id",
+  "source" : "source",
+  "trajData" : [ {
+    "date" : "date",
+    "pres" : 7.061401241503109,
+    "geoLocation" : {
+      "coordinates" : [ 0.8008281904610115, 0.8008281904610115 ],
+      "type" : "type"
+    },
+    "season" : 9.301444243932576,
+    "lon" : 5.637376656633329,
+    "time" : 1.4658129805029452,
+    "class" : "class",
+    "lat" : 5.962133916683182,
+    "wind" : 2.3021358869347655,
+    "timestamp" : "2000-01-23T04:56:07.000+00:00"
+  }, {
+    "date" : "date",
+    "pres" : 7.061401241503109,
+    "geoLocation" : {
+      "coordinates" : [ 0.8008281904610115, 0.8008281904610115 ],
+      "type" : "type"
+    },
+    "season" : 9.301444243932576,
+    "lon" : 5.637376656633329,
+    "time" : 1.4658129805029452,
+    "class" : "class",
+    "lat" : 5.962133916683182,
+    "wind" : 2.3021358869347655,
+    "timestamp" : "2000-01-23T04:56:07.000+00:00"
+  } ],
+  "startDate" : "2000-01-23T04:56:07.000+00:00"
+} ];
     if (Object.keys(examples).length > 0) {
       resolve(examples[Object.keys(examples)[0]]);
     } else {
@@ -135,12 +172,12 @@ exports.findTCbyDate = function(date) {
  *
  * startDate Date date-time formatted string indicating the beginning of a time period
  * endDate Date date-time formatted string indicating the end of a time period
- * returns tcSchema
+ * returns List
  **/
 exports.findTCbyDateRange = function(startDate,endDate) {
   return new Promise(function(resolve, reject) {
     var examples = {};
-    examples['application/json'] = {
+    examples['application/json'] = [ {
   "endDate" : "2000-01-23T04:56:07.000+00:00",
   "year" : 6.027456183070403,
   "num" : 0.8008281904610115,
@@ -177,7 +214,44 @@ exports.findTCbyDateRange = function(startDate,endDate) {
     "timestamp" : "2000-01-23T04:56:07.000+00:00"
   } ],
   "startDate" : "2000-01-23T04:56:07.000+00:00"
-};
+}, {
+  "endDate" : "2000-01-23T04:56:07.000+00:00",
+  "year" : 6.027456183070403,
+  "num" : 0.8008281904610115,
+  "name" : "name",
+  "_id" : "_id",
+  "source" : "source",
+  "trajData" : [ {
+    "date" : "date",
+    "pres" : 7.061401241503109,
+    "geoLocation" : {
+      "coordinates" : [ 0.8008281904610115, 0.8008281904610115 ],
+      "type" : "type"
+    },
+    "season" : 9.301444243932576,
+    "lon" : 5.637376656633329,
+    "time" : 1.4658129805029452,
+    "class" : "class",
+    "lat" : 5.962133916683182,
+    "wind" : 2.3021358869347655,
+    "timestamp" : "2000-01-23T04:56:07.000+00:00"
+  }, {
+    "date" : "date",
+    "pres" : 7.061401241503109,
+    "geoLocation" : {
+      "coordinates" : [ 0.8008281904610115, 0.8008281904610115 ],
+      "type" : "type"
+    },
+    "season" : 9.301444243932576,
+    "lon" : 5.637376656633329,
+    "time" : 1.4658129805029452,
+    "class" : "class",
+    "lat" : 5.962133916683182,
+    "wind" : 2.3021358869347655,
+    "timestamp" : "2000-01-23T04:56:07.000+00:00"
+  } ],
+  "startDate" : "2000-01-23T04:56:07.000+00:00"
+} ];
     if (Object.keys(examples).length > 0) {
       resolve(examples[Object.keys(examples)[0]]);
     } else {
