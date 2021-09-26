3b0c870b48538ead2141f050d2ce49c50ace9789
billmills <you@example.com>
2021-09-26 15:36:29 -0400

    corrections to grid metadata spec

--- nodejs-server/service/GridService.js
+++ nodejs-server/service/GridService.js
@@ -77,18 +77,24 @@ exports.gridCoords = function(gridName,latRange,lonRange) {
  * metadata from grid unique dates, pres levels
  *
  * gridName String name of the gridded product
- * returns GridMeta
+ * returns List
  **/
 exports.gridmeta = function(gridName) {
   return new Promise(function(resolve, reject) {
     var examples = {};
-    examples['application/json'] = {
+    examples['application/json'] = [ {
   "minDate" : "2000-01-23T04:56:07.000+00:00",
   "dates" : [ "2000-01-23T04:56:07.000+00:00", "2000-01-23T04:56:07.000+00:00" ],
   "maxDate" : "2000-01-23T04:56:07.000+00:00",
-  "_id" : "_id",
+  "_id" : "",
   "presLevels" : [ 0.8008281904610115, 0.8008281904610115 ]
-};
+}, {
+  "minDate" : "2000-01-23T04:56:07.000+00:00",
+  "dates" : [ "2000-01-23T04:56:07.000+00:00", "2000-01-23T04:56:07.000+00:00" ],
+  "maxDate" : "2000-01-23T04:56:07.000+00:00",
+  "_id" : "",
+  "presLevels" : [ 0.8008281904610115, 0.8008281904610115 ]
+} ];
     if (Object.keys(examples).length > 0) {
       resolve(examples[Object.keys(examples)[0]]);
     } else {
