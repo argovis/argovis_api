772c840f75cdf22e348919b490394d506baeda8d
billmills <you@example.com>
2021-09-26 14:43:39 -0400

    tc schema corrections

--- nodejs-server/service/TcService.js
+++ nodejs-server/service/TcService.js
@@ -266,12 +266,12 @@ exports.findTCbyDateRange = function(startDate,endDate) {
  *
  * name String name of tropical cyclone
  * year BigDecimal year of tropical cyclone
- * returns tcSchema
+ * returns List
  **/
 exports.findTCbyNameYear = function(name,year) {
   return new Promise(function(resolve, reject) {
     var examples = {};
-    examples['application/json'] = {
+    examples['application/json'] = [ {
   "endDate" : "2000-01-23T04:56:07.000+00:00",
   "year" : 6.027456183070403,
   "num" : 0.8008281904610115,
@@ -308,7 +308,44 @@ exports.findTCbyNameYear = function(name,year) {
     "timestamp" : "2000-01-23T04:56:07.000+00:00"
   } ],
   "startDate" : "2000-01-23T04:56:07.000+00:00"
-};
+}, {
+  "endDate" : "2000-01-23T04:56:07.000+00:00",
+  "year" : 6.027456183070403,
+  "num" : 0.8008281904610115,
+  "name" : "name",
+  "_id" : "_id",
+  "source" : "source",
+  "trajData" : [ {
+    "date" : "date",
+    "pres" : 7.061401241503109,
+    "geoLocation" : {
+      "coordinates" : [ 0.8008281904610115, 0.8008281904610115 ],
+      "type" : "type"
+    },
+    "season" : 9.301444243932576,
+    "lon" : 5.637376656633329,
+    "time" : 1.4658129805029452,
+    "class" : "class",
+    "lat" : 5.962133916683182,
+    "wind" : 2.3021358869347655,
+    "timestamp" : "2000-01-23T04:56:07.000+00:00"
+  }, {
+    "date" : "date",
+    "pres" : 7.061401241503109,
+    "geoLocation" : {
+      "coordinates" : [ 0.8008281904610115, 0.8008281904610115 ],
+      "type" : "type"
+    },
+    "season" : 9.301444243932576,
+    "lon" : 5.637376656633329,
+    "time" : 1.4658129805029452,
+    "class" : "class",
+    "lat" : 5.962133916683182,
+    "wind" : 2.3021358869347655,
+    "timestamp" : "2000-01-23T04:56:07.000+00:00"
+  } ],
+  "startDate" : "2000-01-23T04:56:07.000+00:00"
+} ];
     if (Object.keys(examples).length > 0) {
       resolve(examples[Object.keys(examples)[0]]);
     } else {
