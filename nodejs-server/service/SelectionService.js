cf7cca85df4b3f19323a287acd613b9130884c6d
billmills <you@example.com>
2021-09-28 13:24:35 -0400

    more selection routes

--- nodejs-server/service/SelectionService.js
+++ nodejs-server/service/SelectionService.js
@@ -2,6 +2,48 @@
 
 
 /**
+ * Get a list of profile metadata for all profiles in a given time window.
+ *
+ * startDate Date date-time formatted string indicating the beginning of a time period
+ * endDate Date date-time formatted string indicating the end of a time period
+ * returns List
+ **/
+exports.selectionGlobalMapProfiles = function(startDate,endDate) {
+  return new Promise(function(resolve, reject) {
+    var examples = {};
+    examples['application/json'] = [ {
+  "date" : "2000-01-23T04:56:07.000+00:00",
+  "cycle_number" : 0,
+  "geoLocation" : {
+    "coordinates" : [ 0.8008281904610115, 0.8008281904610115 ],
+    "type" : "type"
+  },
+  "DATA_MODE" : "DATA_MODE",
+  "DIRECTION" : "DIRECTION",
+  "platform_number" : 6,
+  "_id" : "_id"
+}, {
+  "date" : "2000-01-23T04:56:07.000+00:00",
+  "cycle_number" : 0,
+  "geoLocation" : {
+    "coordinates" : [ 0.8008281904610115, 0.8008281904610115 ],
+    "type" : "type"
+  },
+  "DATA_MODE" : "DATA_MODE",
+  "DIRECTION" : "DIRECTION",
+  "platform_number" : 6,
+  "_id" : "_id"
+} ];
+    if (Object.keys(examples).length > 0) {
+      resolve(examples[Object.keys(examples)[0]]);
+    } else {
+      resolve();
+    }
+  });
+}
+
+
+/**
  * Get a list of profiles by ID, keeping only levels within a range of pressures.
  *
  * presRange List Pressure range
