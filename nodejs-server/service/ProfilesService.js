394debbbf3c05015a93edd29220d2f9404f3ae45
billmills <you@example.com>
2021-10-08 15:45:32 -0400

    profile list templates

--- nodejs-server/service/ProfilesService.js
+++ nodejs-server/service/ProfilesService.js
@@ -249,6 +249,33 @@ exports.profile = function(startDate,endDate,polygon,box,ids,platforms,presRange
 
 
 /**
+ * List profile IDs that match a search
+ *
+ * startDate Date date-time formatted string indicating the beginning of a time period (optional)
+ * endDate Date date-time formatted string indicating the end of a time period (optional)
+ * polygon String array of [lon, lat] vertices describing a polygon; final point must match initial point (optional)
+ * box String box described as [[lower left lon, lower left lat], [upper right lon, upper right lat]] (optional)
+ * ids List List of profile IDs (optional)
+ * platforms List List of platform IDs (optional)
+ * presRange List Pressure range (optional)
+ * coreMeasurements List Keys of core measurements to include (optional)
+ * bgcMeasurements List Keys of BGC measurements to include (optional)
+ * returns List
+ **/
+exports.profileList = function(startDate,endDate,polygon,box,ids,platforms,presRange,coreMeasurements,bgcMeasurements) {
+  return new Promise(function(resolve, reject) {
+    var examples = {};
+    examples['application/json'] = [ "", "" ];
+    if (Object.keys(examples).length > 0) {
+      resolve(examples[Object.keys(examples)[0]]);
+    } else {
+      resolve();
+    }
+  });
+}
+
+
+/**
  * Provides a summary of the profile database.
  *
  * returns profileCollectionSummary
