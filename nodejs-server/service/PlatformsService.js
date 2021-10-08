05c9558ec77c3c832c316b541aa89b432a66c048
billmills <you@example.com>
2021-10-08 14:36:08 -0400

    schema corrections

--- nodejs-server/service/PlatformsService.js
+++ nodejs-server/service/PlatformsService.js
@@ -22,12 +22,12 @@ exports.bgcPlatformList = function() {
 /**
  * Provides a list of all platforms with their most recent known report and position.
  *
- * returns platformRecent
+ * returns List
  **/
 exports.platformList = function() {
   return new Promise(function(resolve, reject) {
     var examples = {};
-    examples['application/json'] = {
+    examples['application/json'] = [ {
   "cycle_number" : 5,
   "most_recent_date" : "2000-01-23T04:56:07.000+00:00",
   "geoLocation" : {
@@ -38,7 +38,18 @@ exports.platformList = function() {
   "platform_number" : 6.027456183070403,
   "number_of_profiles" : 1,
   "_id" : 0.8008281904610115
-};
+}, {
+  "cycle_number" : 5,
+  "most_recent_date" : "2000-01-23T04:56:07.000+00:00",
+  "geoLocation" : {
+    "coordinates" : [ 0.8008281904610115, 0.8008281904610115 ],
+    "type" : "type"
+  },
+  "dac" : "dac",
+  "platform_number" : 6.027456183070403,
+  "number_of_profiles" : 1,
+  "_id" : 0.8008281904610115
+} ];
     if (Object.keys(examples).length > 0) {
       resolve(examples[Object.keys(examples)[0]]);
     } else {
