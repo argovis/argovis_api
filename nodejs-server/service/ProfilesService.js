a49b32cb384ea44e330986ab40a561cafc042a3f
billmills <you@example.com>
2021-11-29 16:56:36 -0500

    allow proximity search in /profiles

--- nodejs-server/service/ProfilesService.js
+++ nodejs-server/service/ProfilesService.js
@@ -8,6 +8,8 @@
  * endDate Date date-time formatted string indicating the end of a time period (optional)
  * polygon String array of [lon, lat] vertices describing a polygon; final point must match initial point (optional)
  * box String box described as [[lower left lon, lower left lat], [upper right lon, upper right lat]] (optional)
+ * center List center to measure max radius from (optional)
+ * radius BigDecimal km from centerpoint (optional)
  * ids List List of profile IDs (optional)
  * platforms List List of platform IDs (optional)
  * presRange List Pressure range (optional)
@@ -15,7 +17,7 @@
  * bgcMeasurements List Keys of BGC measurements to include (optional)
  * returns List
  **/
-exports.profile = function(startDate,endDate,polygon,box,ids,platforms,presRange,coreMeasurements,bgcMeasurements) {
+exports.profile = function(startDate,endDate,polygon,box,center,radius,ids,platforms,presRange,coreMeasurements,bgcMeasurements) {
   return new Promise(function(resolve, reject) {
     var examples = {};
     examples['application/json'] = [ {
