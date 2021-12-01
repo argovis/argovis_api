8f583e481d183b69d7eb6f5be117a5e5b88c082a
billmills <you@example.com>
2021-12-01 11:44:46 -0500

    DAC filtering for profiles

--- nodejs-server/controllers/Profiles.js
+++ nodejs-server/controllers/Profiles.js
@@ -13,8 +13,8 @@ module.exports.profile = function profile (req, res, next, startDate, endDate, p
     });
 };
 
-module.exports.profileList = function profileList (req, res, next, startDate, endDate, polygon, box, center, radius, platforms, presRange, coreMeasurements, bgcMeasurements) {
-  Profiles.profileList(startDate, endDate, polygon, box, center, radius, platforms, presRange, coreMeasurements, bgcMeasurements)
+module.exports.profileList = function profileList (req, res, next, startDate, endDate, polygon, box, center, radius, param, platforms, presRange, coreMeasurements, bgcMeasurements) {
+  Profiles.profileList(startDate, endDate, polygon, box, center, radius, param, platforms, presRange, coreMeasurements, bgcMeasurements)
     .then(function (response) {
       utils.writeJson(res, response);
     })
