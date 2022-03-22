d622f2b47aed148106beb24e7287e592b8ed5a9f
billmills <you@example.com>
2022-03-22 13:33:33 -0400

    one id filter per profiles request

--- nodejs-server/service/ProfilesService.js
+++ nodejs-server/service/ProfilesService.js
@@ -36,7 +36,7 @@ exports.profile = function(startDate,endDate,polygon,box,center,radius,id,platfo
     //   return; 
     // } 
 
-    let aggPipeline = profile_candidate_agg_pipeline(startDate,endDate,polygon,box,center,radius,ids,platforms,dac,source, woceline, datakey)
+    let aggPipeline = profile_candidate_agg_pipeline(startDate,endDate,polygon,box,center,radius,id,platforms,dac,source, woceline, datakey)
 
     if('code' in aggPipeline){
       reject(aggPipeline);
@@ -298,7 +298,7 @@ const reinflate = function(profile){
   return profile
 }
 
-const profile_candidate_agg_pipeline = function(startDate,endDate,polygon,box,center,radius,ids,platforms,dac,source,woceline,datakey){
+const profile_candidate_agg_pipeline = function(startDate,endDate,polygon,box,center,radius,id,platforms,dac,source,woceline,datakey){
     // return an aggregation pipeline array that describes how we want to filter eligible profiles
     // in case of error, return the object to pass to reject().
 
@@ -329,8 +329,8 @@ const profile_candidate_agg_pipeline = function(startDate,endDate,polygon,box,ce
       aggPipeline.push({$match:  {timestamp: {$lte: endDate}}})
     }
 
-    if (ids){
-      aggPipeline.push({$match: {_id: { $in: ids}}})
+    if(id){
+      aggPipeline.push({ $match : { _id : id } })
     }
 
     if(platforms) {
