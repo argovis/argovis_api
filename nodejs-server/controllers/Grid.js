e3a24d38f2aa96052ad274079cc88b85b8cca280
billmills <you@example.com>
2022-04-25 13:46:37 -0400

    debugging

--- nodejs-server/controllers/Grid.js
+++ nodejs-server/controllers/Grid.js
@@ -5,6 +5,11 @@ var Grid = require('../service/GridService');
 
 
 module.exports.gridselect = function gridselect (req, res, next, gridName, polygon, startDate, endDate, presRange) {
+  console.log('controller grid:', gridName)
+  console.log('controller poly:', polygon)
+  console.log('controller start:', startDate)
+  console.log('controller end:', endDate)
+  console.log('controller pres:', presRange)
   Grid.gridselect(gridName, polygon, startDate, endDate, presRange)
     .then(function (response) {
       utils.writeJson(res, response);
--- nodejs-server/service/GridService.js
+++ nodejs-server/service/GridService.js
@@ -18,7 +18,6 @@ const datePresGrouping = {_id: '$gridName', presLevels: {$addToSet: '$pres'}, da
  **/
 exports.gridselect = function(gridName,polygon,startDate,endDate,presRange) {
   return new Promise(function(resolve, reject) {
-
     // sanitation
     if(!(gridName in Grid)){
       return {"code": 400, "message": gridName + " is not a supported grid; instead try one of: " + Grid.keys().join()};  
@@ -39,7 +38,7 @@ exports.gridselect = function(gridName,polygon,startDate,endDate,presRange) {
     if(!GJV.valid(polygon)){
       return {"code": 400, "message": "Polygon region wasn't proper geoJSON; format should be [[lon,lat],[lon,lat],...]"};
     }
-
+    console.log(Grid[gridName])
     let query = Grid[gridName].aggregate([{$match: {"g": {$geoWithin: {$geometry: polygon}}, "t": {$gte: startDate, $lte: endDate} }}])
 
     query.exec(helpers.queryCallback.bind(null,null, resolve, reject))
