8e7250eba8ea9296952f2234b9c559f91a3b741b
billmills <you@example.com>
2022-04-27 18:32:05 -0400

    metadata only option for grids

--- nodejs-server/service/GridService.js
+++ nodejs-server/service/GridService.js
@@ -10,16 +10,16 @@ const datePresGrouping = {_id: '$gridName', presLevels: {$addToSet: '$pres'}, da
  * gridded product selector
  *
  * gridName String name of the gridded product
+ * polygon String array of [lon, lat] vertices describing a polygon; final point must match initial point
+ * startDate Date date-time formatted string indicating the beginning of a time period
+ * endDate Date date-time formatted string indicating the end of a time period
  * presRange List Pressure range (optional)
- * polygon String array of [lon, lat] vertices describing a polygon; final point must match initial point (optional)
- * startDate Date date-time formatted string indicating the beginning of a time period (optional)
- * endDate Date date-time formatted string indicating the end of a time period (optional)
  * returns List
  **/
-exports.gridselect = function(gridName,presRange,polygon,startDate,endDate) {
+exports.gridselect = function(gridName,polygon,startDate,endDate,presRange) {
   return new Promise(function(resolve, reject) {
 
-    if(gridName && !polygon && !startDate && !endDate && !presRange){
+    if(gridName && (typeof polygon == 'undefined' && typeof startDate == 'undefined' && typeof endDate == 'undefined' && typeof presRange == 'undefined')){
         // metadata only request
         const query = Grid['grids-meta'].aggregate([{$match:{"_id": gridName}}]);
 
@@ -36,49 +36,54 @@ exports.gridselect = function(gridName,presRange,polygon,startDate,endDate) {
 
           resolve(meta[0]);
         })
-    }
-
-    // sanitation
-    startDate = new Date(startDate);
-    endDate = new Date(endDate);
-
-    if(!(gridName in Grid)){
-      reject({"code": 400, "message": gridName + " is not a supported grid; instead try one of: " + Object.getOwnPropertyNames(Grid)});  
-    }
-
-    try {
-      polygon = JSON.parse(polygon);
-    } catch (e) {
-      reject({"code": 400, "message": "Polygon region wasn't proper JSON; format should be [[lon,lat],[lon,lat],...]"});
-    }
-    if(!helpers.validlonlat(polygon)){
-      reject({"code": 400, "message": "All lon, lat pairs must respect -180<=lon<=180 and -90<=lat<-90"}); 
-    }
-    polygon = {
-      "type": "Polygon",
-      "coordinates": [polygon]
-    }
-    if(!GJV.valid(polygon)){
-      reject({"code": 400, "message": "Polygon region wasn't proper geoJSON; format should be [[lon,lat],[lon,lat],...]"});
-    }
-
-    Promise.all([
-        Grid[gridName].aggregate([{$match: {"g": {$geoWithin: {$geometry: polygon}}, "t": {$gte: startDate, $lte: endDate} }}]),
-        Grid['grids-meta'].aggregate([{$match:{"_id": gridName}}])
-    ]).then( ([ grids, gridmeta]) => {
-
-        if(presRange){
-            for(let i=0; i<grids.length; i++){
-                grids[i]['d'] = grids[i]['d'].filter((e,i) => gridmeta[0]['levels'][i]>presRange[0] && gridmeta[0]['levels'][i]<presRange[1])
-            }
-            gridmeta[0]['levels'] = gridmeta[0]['levels'].filter(x => x>presRange[0] && x<presRange[1])
+    } else {
+        // regular data request
+        // sanitation
+        if(!polygon || !startDate || !endDate || !gridName){
+            reject({"code": 400, "message": "Query string parameters gridName, polygon, startDate, and endDate are all required unless you are making a metadata-only request, in which case only gridName should be provided." }); 
         }
-        grids.unshift(gridmeta[0])
-        resolve(grids);
-    }).catch(error => {
-        reject({"code": 500, "message": "Server error"});
-        return;
-    });
+
+        startDate = new Date(startDate);
+        endDate = new Date(endDate);
+
+        if(!(gridName in Grid)){
+          reject({"code": 400, "message": gridName + " is not a supported grid; instead try one of: " + Object.getOwnPropertyNames(Grid)});  
+        }
+
+        try {
+          polygon = JSON.parse(polygon);
+        } catch (e) {
+          reject({"code": 400, "message": "Polygon region wasn't proper JSON; format should be [[lon,lat],[lon,lat],...]"});
+        }
+        if(!helpers.validlonlat(polygon)){
+          reject({"code": 400, "message": "All lon, lat pairs must respect -180<=lon<=180 and -90<=lat<-90"}); 
+        }
+        polygon = {
+          "type": "Polygon",
+          "coordinates": [polygon]
+        }
+        if(!GJV.valid(polygon)){
+          reject({"code": 400, "message": "Polygon region wasn't proper geoJSON; format should be [[lon,lat],[lon,lat],...]"});
+        }
+
+        Promise.all([
+            Grid[gridName].aggregate([{$match: {"g": {$geoWithin: {$geometry: polygon}}, "t": {$gte: startDate, $lte: endDate} }}]),
+            Grid['grids-meta'].aggregate([{$match:{"_id": gridName}}])
+        ]).then( ([ grids, gridmeta]) => {
+
+            if(presRange){
+                for(let i=0; i<grids.length; i++){
+                    grids[i]['d'] = grids[i]['d'].filter((e,i) => gridmeta[0]['levels'][i]>presRange[0] && gridmeta[0]['levels'][i]<presRange[1])
+                }
+                gridmeta[0]['levels'] = gridmeta[0]['levels'].filter(x => x>presRange[0] && x<presRange[1])
+            }
+            grids.unshift(gridmeta[0])
+            resolve(grids);
+        }).catch(error => {
+            reject({"code": 500, "message": "Server error"});
+            return;
+        });
+    }   
   });
 }
 
