803affbd6cfa41381ed9dba865738b8885e5a5b4
billmills <you@example.com>
2022-05-11 16:17:33 -0400

    multipolygons for grid routes

--- nodejs-server/service/GridService.js
+++ nodejs-server/service/GridService.js
@@ -7,11 +7,12 @@
  * gridName String name of the gridded product
  * presRange List Pressure range (optional)
  * polygon String array of [lon, lat] vertices describing a polygon; final point must match initial point (optional)
+ * multipolygon String array of polygon regions; will return points interior to all listed polygons (optional)
  * startDate Date date-time formatted string indicating the beginning of a time period (optional)
  * endDate Date date-time formatted string indicating the end of a time period (optional)
  * returns List
  **/
-exports.gridselect = function(gridName,presRange,polygon,startDate,endDate) {
+exports.gridselect = function(gridName,presRange,polygon,multipolygon,startDate,endDate) {
   return new Promise(function(resolve, reject) {
     var examples = {};
     examples['application/json'] = [ {
