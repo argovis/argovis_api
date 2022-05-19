a26146cce381e2e7b473759343829baad7a080ac
billmills <you@example.com>
2022-05-19 19:55:31 -0400

    dacs logic

--- nodejs-server/service/DacsService.js
+++ nodejs-server/service/DacsService.js
@@ -1,5 +1,5 @@
 'use strict';
-
+const Profile = require('../models/profile');
 
 /**
  * Provides summary data for each data assembly center.
@@ -8,7 +8,26 @@
  **/
 exports.dacSummary = function() {
   return new Promise(function(resolve, reject) {
-     reject({"code": 501, "message": "Not implemented"});
+
+    const sortMap = new Map();
+    sortMap.set('data_center', 1)
+    sortMap.set('timestamp', -1)
+
+    let dacsummary = [
+        {'$sort': sortMap}, 
+        {'$group': {'_id': '$data_center','number_of_profiles': {'$sum':1}, 'most_recent_date':{'$first':'$timestamp'}}}  
+    ]
+
+    const query = Profile.aggregate(dacsummary)
+
+    query.exec(function (err, dacs) {
+      if (err){
+        reject({"code": 500, "message": "Server error"});
+        return;
+      }
+      resolve(dacs);
+    })
   });
 }
+
 
