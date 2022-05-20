dc64d4639314dbd4f91b205414d6aebc181e57a4
billmills <you@example.com>
2022-05-20 15:51:02 -0400

    platform bgc controller boilerplate

--- nodejs-server/controllers/Platforms.js
+++ nodejs-server/controllers/Platforms.js
@@ -7,6 +7,9 @@ module.exports.platformBGC = function platformBGC (req, res, next) {
   Platforms.platformBGC()
     .then(function (response) {
       utils.writeJson(res, response);
+    }, 
+    function (response) {
+      utils.writeJson(res, response, response.code);
     })
     .catch(function (response) {
       utils.writeJson(res, response);
