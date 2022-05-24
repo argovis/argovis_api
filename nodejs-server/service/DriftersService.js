0ff8a71c454b428e5289687dab67d34f740815f5
billmills <you@example.com>
2022-05-23 23:29:08 -0400

    rename logic function for /drifters/meta

--- nodejs-server/service/DriftersService.js
+++ nodejs-server/service/DriftersService.js
@@ -2,6 +2,78 @@
 
 
 /**
+ * Search, reduce and download drifter metadata.
+ *
+ * id String Unique drifter ID to search for. (optional)
+ * wmo BigDecimal World Meteorological Organization identification number (optional)
+ * returns List
+ **/
+exports.drifterMetaSearch = function(id,wmo) {
+  return new Promise(function(resolve, reject) {
+    var examples = {};
+    examples['application/json'] = [ {
+  "end_date" : "2000-01-23T04:56:07.000+00:00",
+  "typebuoy" : "typebuoy",
+  "end_lat" : 7.061401241503109,
+  "deploy_lon" : 5.962133916683182,
+  "drogue_lost_date" : "2000-01-23T04:56:07.000+00:00",
+  "rowsize" : 0.8008281904610115,
+  "units" : [ "units", "units" ],
+  "date_updated_argovis" : "2000-01-23T04:56:07.000+00:00",
+  "typedeath" : 9.301444243932576,
+  "long_name" : [ "long_name", "long_name" ],
+  "deploy_date" : "2000-01-23T04:56:07.000+00:00",
+  "deploy_lat" : 5.637376656633329,
+  "WMO" : 6.027456183070403,
+  "data_type" : "data_type",
+  "expno" : 1.4658129805029452,
+  "data_keys" : [ "data_keys", "data_keys" ],
+  "_id" : "_id",
+  "source_info" : [ {
+    "source" : [ "source", "source" ],
+    "source_url" : "source_url"
+  }, {
+    "source" : [ "source", "source" ],
+    "source_url" : "source_url"
+  } ],
+  "end_lon" : 2.3021358869347655
+}, {
+  "end_date" : "2000-01-23T04:56:07.000+00:00",
+  "typebuoy" : "typebuoy",
+  "end_lat" : 7.061401241503109,
+  "deploy_lon" : 5.962133916683182,
+  "drogue_lost_date" : "2000-01-23T04:56:07.000+00:00",
+  "rowsize" : 0.8008281904610115,
+  "units" : [ "units", "units" ],
+  "date_updated_argovis" : "2000-01-23T04:56:07.000+00:00",
+  "typedeath" : 9.301444243932576,
+  "long_name" : [ "long_name", "long_name" ],
+  "deploy_date" : "2000-01-23T04:56:07.000+00:00",
+  "deploy_lat" : 5.637376656633329,
+  "WMO" : 6.027456183070403,
+  "data_type" : "data_type",
+  "expno" : 1.4658129805029452,
+  "data_keys" : [ "data_keys", "data_keys" ],
+  "_id" : "_id",
+  "source_info" : [ {
+    "source" : [ "source", "source" ],
+    "source_url" : "source_url"
+  }, {
+    "source" : [ "source", "source" ],
+    "source_url" : "source_url"
+  } ],
+  "end_lon" : 2.3021358869347655
+} ];
+    if (Object.keys(examples).length > 0) {
+      resolve(examples[Object.keys(examples)[0]]);
+    } else {
+      resolve();
+    }
+  });
+}
+
+
+/**
  * Search, reduce and download drifter data.
  *
  * startDate Date ISO 8601 UTC date-time formatted string indicating the beginning of the time period of interest. (optional)
@@ -34,78 +106,6 @@ exports.drifterSearch = function(startDate,endDate,polygon,id,wmo) {
     "type" : "type"
   },
   "timestamp" : "2000-01-23T04:56:07.000+00:00"
-} ];
-    if (Object.keys(examples).length > 0) {
-      resolve(examples[Object.keys(examples)[0]]);
-    } else {
-      resolve();
-    }
-  });
-}
-
-
-/**
- * Search, reduce and download drifter metadata.
- *
- * id String Unique drifter ID to search for. (optional)
- * wmo BigDecimal World Meteorological Organization identification number (optional)
- * returns List
- **/
-exports.drifterSearch_0 = function(id,wmo) {
-  return new Promise(function(resolve, reject) {
-    var examples = {};
-    examples['application/json'] = [ {
-  "end_date" : "2000-01-23T04:56:07.000+00:00",
-  "typebuoy" : "typebuoy",
-  "end_lat" : 7.061401241503109,
-  "deploy_lon" : 5.962133916683182,
-  "drogue_lost_date" : "2000-01-23T04:56:07.000+00:00",
-  "rowsize" : 0.8008281904610115,
-  "units" : [ "units", "units" ],
-  "date_updated_argovis" : "2000-01-23T04:56:07.000+00:00",
-  "typedeath" : 9.301444243932576,
-  "long_name" : [ "long_name", "long_name" ],
-  "deploy_date" : "2000-01-23T04:56:07.000+00:00",
-  "deploy_lat" : 5.637376656633329,
-  "WMO" : 6.027456183070403,
-  "data_type" : "data_type",
-  "expno" : 1.4658129805029452,
-  "data_keys" : [ "data_keys", "data_keys" ],
-  "_id" : "_id",
-  "source_info" : [ {
-    "source" : [ "source", "source" ],
-    "source_url" : "source_url"
-  }, {
-    "source" : [ "source", "source" ],
-    "source_url" : "source_url"
-  } ],
-  "end_lon" : 2.3021358869347655
-}, {
-  "end_date" : "2000-01-23T04:56:07.000+00:00",
-  "typebuoy" : "typebuoy",
-  "end_lat" : 7.061401241503109,
-  "deploy_lon" : 5.962133916683182,
-  "drogue_lost_date" : "2000-01-23T04:56:07.000+00:00",
-  "rowsize" : 0.8008281904610115,
-  "units" : [ "units", "units" ],
-  "date_updated_argovis" : "2000-01-23T04:56:07.000+00:00",
-  "typedeath" : 9.301444243932576,
-  "long_name" : [ "long_name", "long_name" ],
-  "deploy_date" : "2000-01-23T04:56:07.000+00:00",
-  "deploy_lat" : 5.637376656633329,
-  "WMO" : 6.027456183070403,
-  "data_type" : "data_type",
-  "expno" : 1.4658129805029452,
-  "data_keys" : [ "data_keys", "data_keys" ],
-  "_id" : "_id",
-  "source_info" : [ {
-    "source" : [ "source", "source" ],
-    "source_url" : "source_url"
-  }, {
-    "source" : [ "source", "source" ],
-    "source_url" : "source_url"
-  } ],
-  "end_lon" : 2.3021358869347655
 } ];
     if (Object.keys(examples).length > 0) {
       resolve(examples[Object.keys(examples)[0]]);
