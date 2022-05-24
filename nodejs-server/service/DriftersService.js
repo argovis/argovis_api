0cf6382274f41475f953d03e4e6eb9168ff1c286
billmills <you@example.com>
2022-05-23 23:32:05 -0400

    drifter logic boilerplate

--- nodejs-server/service/DriftersService.js
+++ nodejs-server/service/DriftersService.js
@@ -1,5 +1,5 @@
 'use strict';
-
+const Drifter = require('../models/drifter');
 
 /**
  * Search, reduce and download drifter metadata.
@@ -10,65 +10,7 @@
  **/
 exports.drifterMetaSearch = function(id,wmo) {
   return new Promise(function(resolve, reject) {
-    var examples = {};
-    examples['application/json'] = [ {
-  "end_date" : "2000-01-23T04:56:07.000+00:00",
-  "typebuoy" : "typebuoy",
-  "end_lat" : 7.061401241503109,
-  "deploy_lon" : 5.962133916683182,
-  "drogue_lost_date" : "2000-01-23T04:56:07.000+00:00",
-  "rowsize" : 0.8008281904610115,
-  "units" : [ "units", "units" ],
-  "date_updated_argovis" : "2000-01-23T04:56:07.000+00:00",
-  "typedeath" : 9.301444243932576,
-  "long_name" : [ "long_name", "long_name" ],
-  "deploy_date" : "2000-01-23T04:56:07.000+00:00",
-  "deploy_lat" : 5.637376656633329,
-  "WMO" : 6.027456183070403,
-  "data_type" : "data_type",
-  "expno" : 1.4658129805029452,
-  "data_keys" : [ "data_keys", "data_keys" ],
-  "_id" : "_id",
-  "source_info" : [ {
-    "source" : [ "source", "source" ],
-    "source_url" : "source_url"
-  }, {
-    "source" : [ "source", "source" ],
-    "source_url" : "source_url"
-  } ],
-  "end_lon" : 2.3021358869347655
-}, {
-  "end_date" : "2000-01-23T04:56:07.000+00:00",
-  "typebuoy" : "typebuoy",
-  "end_lat" : 7.061401241503109,
-  "deploy_lon" : 5.962133916683182,
-  "drogue_lost_date" : "2000-01-23T04:56:07.000+00:00",
-  "rowsize" : 0.8008281904610115,
-  "units" : [ "units", "units" ],
-  "date_updated_argovis" : "2000-01-23T04:56:07.000+00:00",
-  "typedeath" : 9.301444243932576,
-  "long_name" : [ "long_name", "long_name" ],
-  "deploy_date" : "2000-01-23T04:56:07.000+00:00",
-  "deploy_lat" : 5.637376656633329,
-  "WMO" : 6.027456183070403,
-  "data_type" : "data_type",
-  "expno" : 1.4658129805029452,
-  "data_keys" : [ "data_keys", "data_keys" ],
-  "_id" : "_id",
-  "source_info" : [ {
-    "source" : [ "source", "source" ],
-    "source_url" : "source_url"
-  }, {
-    "source" : [ "source", "source" ],
-    "source_url" : "source_url"
-  } ],
-  "end_lon" : 2.3021358869347655
-} ];
-    if (Object.keys(examples).length > 0) {
-      resolve(examples[Object.keys(examples)[0]]);
-    } else {
-      resolve();
-    }
+
   });
 }
 
@@ -85,33 +27,7 @@ exports.drifterMetaSearch = function(id,wmo) {
  **/
 exports.drifterSearch = function(startDate,endDate,polygon,id,wmo) {
   return new Promise(function(resolve, reject) {
-    var examples = {};
-    examples['application/json'] = [ {
-  "metadata" : "metadata",
-  "data" : [ 6.027456183070403, 6.027456183070403 ],
-  "_id" : "_id",
-  "basin" : 0.8008281904610115,
-  "geolocation" : {
-    "coordinates" : [ 0.8008281904610115, 0.8008281904610115 ],
-    "type" : "type"
-  },
-  "timestamp" : "2000-01-23T04:56:07.000+00:00"
-}, {
-  "metadata" : "metadata",
-  "data" : [ 6.027456183070403, 6.027456183070403 ],
-  "_id" : "_id",
-  "basin" : 0.8008281904610115,
-  "geolocation" : {
-    "coordinates" : [ 0.8008281904610115, 0.8008281904610115 ],
-    "type" : "type"
-  },
-  "timestamp" : "2000-01-23T04:56:07.000+00:00"
-} ];
-    if (Object.keys(examples).length > 0) {
-      resolve(examples[Object.keys(examples)[0]]);
-    } else {
-      resolve();
-    }
+
   });
 }
 
