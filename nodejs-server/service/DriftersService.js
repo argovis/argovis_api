75071866e143aafd178f8d89a64027e1c80a596f
billmills <you@example.com>
2022-05-25 23:04:17 -0400

    first pass at drifter logic

--- nodejs-server/service/DriftersService.js
+++ nodejs-server/service/DriftersService.js
@@ -1,5 +1,6 @@
 'use strict';
 const Drifter = require('../models/drifter');
+const helpers = require('./helpers')
 
 /**
  * Search, reduce and download drifter metadata.
@@ -10,6 +11,19 @@ const Drifter = require('../models/drifter');
  **/
 exports.drifterMetaSearch = function(id,wmo) {
   return new Promise(function(resolve, reject) {
+
+    let metaMatch = {}
+
+    if(id){
+      metaMatch['_id'] = id
+    }
+
+    if(wmo){
+      metaMatch['WMO'] = wmo
+    }
+
+    let query = Drifter['drifterMeta'].aggregate([{$match: metaMatch}])
+    query.exec(helpers.queryCallback.bind(null,null, resolve, reject))
 
   });
 }
@@ -27,6 +41,78 @@ exports.drifterMetaSearch = function(id,wmo) {
  **/
 exports.drifterSearch = function(startDate,endDate,polygon,id,wmo) {
   return new Promise(function(resolve, reject) {
+
+    if(id && wmo){
+      reject({code:400, message: 'Please filter by at most one of id or wmo.'})
+      return
+    }
+
+    let metaMatch = {}
+    let spacetimeMatch = {}
+    let aggPipeline = []
+
+    // construct metadata match
+    if(id){
+      metaMatch['metadata'] = id
+    }
+
+    // spacetime match
+    /// spacetime sanitation
+    if(startDate){
+      startDate = new Date(startDate);
+    }
+    if(endDate){
+      endDate = new Date(endDate);
+    }
+    if(polygon){
+      polygon = helpers.polygon_sanitation(polygon)
+
+      if(polygon.hasOwnProperty('code')){
+        // error, return and bail out
+        reject(polygon)
+        return
+      }
+    }
+    let bailout = helpers.request_sanitation(startDate, endDate, polygon, null, null, null, null, id, wmo) // wmo here is a bit kludgy; at time of writing, that argument was intended as Argo platform number, and the sanitation funciton waves through anything with a unique platform number, which is also appropriate for a unique WMO number, but not strictly semantically correct.
+    if(bailout){
+      //request looks huge or malformed, reject it
+      reject(bailout)
+      return
+    }
+    /// spacetime agg stage construction
+    if (startDate && endDate){
+      spacetimeMatch['timestamp'] = {$gte: startDate, $lte: endDate}
+    } else if (startDate){
+      spacetimeMatch['timestamp'] = {$gte: startDate}
+    } else if (endDate){
+      spacetimeMatch['timestamp'] = {$lte: endDate}
+    }
+    if(polygon) {
+      spacetimeMatch['geolocation'] = {$geoWithin: {$geometry: polygon}}
+    }
+
+    if(wmo){
+      let query = Drifter['drifterMeta'].aggregate([{$match: {'WMO': wmo}}])
+      query.exec(function (err, driftermeta) {
+        if (err){
+          reject({"code": 500, "message": "Server error"});
+          return;
+        }
+        let ids = new Set(driftermeta.map(x => x['_id']))
+
+        aggPipeline.push({$match:{'metadata':{$in:Array.from(ids)}}})
+        aggPipeline.push({$match: spacetimeMatch})
+        aggPipeline.push({$sort: {timestamp:-1}})
+        query = Drifter['drifters'].aggregate(aggPipeline);
+        query.exec(helpers.queryCallback.bind(null,null, resolve, reject))  
+      })
+    } else {
+      aggPipeline.push({$match: metaMatch})
+      aggPipeline.push({$match: spacetimeMatch})
+      aggPipeline.push({$sort: {timestamp:-1}})    
+      let query = Drifter['drifters'].aggregate(aggPipeline);
+      query.exec(helpers.queryCallback.bind(null,null, resolve, reject))  
+    }
 
   });
 }
