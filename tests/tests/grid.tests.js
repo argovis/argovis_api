af5e8ec540c459e7a1bd17d0a6c0c6df04fb2eac
billmills <you@example.com>
2022-07-21 15:02:50 -0400

    port goship data management tests to grids

--- tests/tests/grid.tests.js
+++ tests/tests/grid.tests.js
@@ -10,6 +10,94 @@ $RefParser.dereference(rawspec, (err, schema) => {
     console.error(err);
   }
   else {
+
+    describe("GET /grids/ohc_kg", function () {
+      it("searches for grids, dont request data", async function () {
+        const response = await request.get("/grids/ohc_kg?polygon=[[107,-64],[108,-64],[108,-65],[107,-65],[107,-64]]").set({'x-argokey': 'developer'});
+        expect(response.body).to.be.jsonSchema(schema.paths['/grids/{gridName}'].get.responses['200'].content['application/json'].schema);
+      });
+    });
+
+    describe("GET /grids/ohc_kg", function () {
+      it("searches for grids profiles with data=metadata-only", async function () {
+        const response = await request.get("/grids/ohc_kg?polygon=[[107,-64],[108,-64],[108,-65],[107,-65],[107,-64]]&data=metadata-only").set({'x-argokey': 'developer'});
+        expect(response.body).to.be.jsonSchema(schema.paths['/grids/{gridName}'].get.responses['200'].content['application/json'].schema);
+      });
+    });
+
+    describe("GET /grids/ohc_kg", function () {
+      it("grids with data filter should return grids-consistent data", async function () {
+        const response = await request.get("/grids/ohc_kg?polygon=[[107,-64],[108,-64],[108,-65],[107,-65],[107,-64]]&data=ohc_kg").set({'x-argokey': 'developer'});
+        expect(response.body).to.be.jsonSchema(schema.paths['/grids/{gridName}'].get.responses['200'].content['application/json'].schema);
+      });
+    });
+
+    describe("GET /grids/ohc_kg", function () {
+      it("grids with data filter should return correct data_keys", async function () {
+        const response = await request.get("/grids/ohc_kg?polygon=[[107,-64],[108,-64],[108,-65],[107,-65],[107,-64]]&data=ohc_kg").set({'x-argokey': 'developer'});
+        expect(response.body[0].data_keys).to.have.members(['ohc_kg'])
+      });
+    });
+
+    describe("GET /grids/ohc_kg", function () {
+      it("grids with data filter should return correct units", async function () {
+        const response = await request.get("/grids/ohc_kg?polygon=[[107,-64],[108,-64],[108,-65],[107,-65],[107,-64]]&data=ohc_kg").set({'x-argokey': 'developer'});
+        expect(response.body[0].units).to.deep.equal({'ohc_kg':"J/m^2"})
+      });
+    });
+
+    describe("GET /grids/ohc_kg", function () {
+      it("grids with data filter should return correct units, as a list when compressed", async function () {
+        const response = await request.get("/grids/ohc_kg?polygon=[[107,-64],[108,-64],[108,-65],[107,-65],[107,-64]]&data=ohc_kg&compression=basic").set({'x-argokey': 'developer'});
+        expect(response.body[0].units).to.deep.equal(["J/m^2"])
+      });
+    });
+
+    describe("GET /grids/ohc_kg", function () {
+      it("grids with data=all filter should return grids-consistent data", async function () {
+        const response = await request.get("/grids/ohc_kg?polygon=[[107,-64],[108,-64],[108,-65],[107,-65],[107,-64]]&data=all").set({'x-argokey': 'developer'});
+        expect(response.body).to.be.jsonSchema(schema.paths['/grids/{gridName}'].get.responses['200'].content['application/json'].schema);
+      });
+    });
+
+    describe("GET /grids/ohc_kg", function () {
+      it("grids with data=all filter should return correct data_keys", async function () {
+        const response = await request.get("/grids/ohc_kg?polygon=[[107,-64],[108,-64],[108,-65],[107,-65],[107,-64]]&data=all").set({'x-argokey': 'developer'});
+        expect(response.body[0].data_keys).to.have.members(['ohc_kg'])
+      });
+    });
+
+    describe("GET /grids/ohc_kg", function () {
+      it("grids with data=all filter should return correct units", async function () {
+        const response = await request.get("/grids/ohc_kg?polygon=[[107,-64],[108,-64],[108,-65],[107,-65],[107,-64]]&data=all").set({'x-argokey': 'developer'});
+        expect(response.body[0].units).to.deep.equal({"ohc_kg":"J/m^2"})
+      });
+    });
+
+    describe("GET /grids/ohc_kg", function () {
+      it("grids profile should be dropped if no requested data is available", async function () {
+        const response = await request.get("/grids/ohc_kg?id=20050115000000_120.5_-64.5&data=ohc_kg").set({'x-argokey': 'developer'});
+        expect(response.status).to.eql(404);
+      });
+    });
+
+    describe("GET /grids/temperature_rg", function () {
+      it("grids with data=all and presRange filters should return correct data", async function () {
+        const response = await request.get("/grids/temperature_rg?id=20040115000000_29.5_-64.5&data=all&presRange=5,100").set({'x-argokey': 'developer'});
+        expect(response.body[0].data[0].temperature_rg).to.eql(-0.017)
+      });
+    });
+
+    describe("GET /grids/temperature_rg", function () {
+      it("grids with data=all and presRange filters should report levels, post filtering, on the data document", async function () {
+        const response = await request.get("/grids/temperature_rg?id=20040115000000_29.5_-64.5&data=all&presRange=5,100").set({'x-argokey': 'developer'});
+        expect(response.body[0].levels).to.deep.equal([ 10, 20, 30, 40, 50, 60, 70, 80, 90, 100])
+      });
+    });
+
+
+
+
     describe("GET /grids/ohc_kg", function () {
       it("fetch gridded data", async function () {
         const response = await request.get("/grids/ohc_kg?polygon=[[112,-65],[112,-64],[114,-64],[114,-65],[112,-65]]&startDate=2000-01-01T00:00:00Z&endDate=2020-01-01T00:00:00Z&data=ohc_kg").set({'x-argokey': 'developer'});
