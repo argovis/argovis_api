084bf010db48d11bbf7f1edbe223658b0cf443dd
billmills <you@example.com>
2023-04-25 13:57:14 -0400

    ping logic

--- nodejs-server/controllers/Ping.js
+++ nodejs-server/controllers/Ping.js
@@ -6,7 +6,10 @@ var Ping = require('../service/PingService');
 module.exports.ping = function ping (req, res, next) {
   Ping.ping()
     .then(function (response) {
-      utils.writeJson(res, response);
+      utils.writeJson(res, response, 200);
+    },
+    function (response) {
+      utils.writeJson(res, response, response.code);
     })
     .catch(function (response) {
       utils.writeJson(res, response);
--- nodejs-server/service/PingService.js
+++ nodejs-server/service/PingService.js
@@ -8,13 +8,8 @@
  **/
 exports.ping = function() {
   return new Promise(function(resolve, reject) {
-    var examples = {};
-    examples['application/json'] = "";
-    if (Object.keys(examples).length > 0) {
-      resolve(examples[Object.keys(examples)[0]]);
-    } else {
-      resolve();
-    }
+      resolve('OK')
+      return
   });
 }
 
