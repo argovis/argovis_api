b2fbda433017183ba00ecb1a66e74971749f87f5
billmills <you@example.com>
2023-04-25 14:23:18 -0400

    plaintext response to ping endpoint

--- nodejs-server/service/PingService.js
+++ nodejs-server/service/PingService.js
@@ -9,7 +9,7 @@
 exports.ping = function() {
   return new Promise(function(resolve, reject) {
     var examples = {};
-    examples['application/json'] = "";
+    examples['application/json'] = "OK";
     if (Object.keys(examples).length > 0) {
       resolve(examples[Object.keys(examples)[0]]);
     } else {
