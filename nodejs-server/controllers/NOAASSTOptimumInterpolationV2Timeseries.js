215dddd6a0e018c129009eaaa5c7e36eaecf64b1
billmills <you@example.com>
2023-06-09 20:49:56 -0400

    draft of NOAA SST templates

---
+++ nodejs-server/controllers/NOAASSTOptimumInterpolationV2Timeseries.js
@@ -0,0 +1,24 @@
+'use strict';
+
+var utils = require('../utils/writer.js');
+var NOAASSTOptimumInterpolationV2Timeseries = require('../service/NOAASSTOptimumInterpolationV2TimeseriesService');
+
+module.exports.findNOAASST = function findNOAASST (req, res, next, id, polygon, multipolygon, winding, center, radius, mostrecent, compression, data) {
+  NOAASSTOptimumInterpolationV2Timeseries.findNOAASST(id, polygon, multipolygon, winding, center, radius, mostrecent, compression, data)
+    .then(function (response) {
+      utils.writeJson(res, response);
+    })
+    .catch(function (response) {
+      utils.writeJson(res, response);
+    });
+};
+
+module.exports.findNOAASSTmeta = function findNOAASSTmeta (req, res, next, id) {
+  NOAASSTOptimumInterpolationV2Timeseries.findNOAASSTmeta(id)
+    .then(function (response) {
+      utils.writeJson(res, response);
+    })
+    .catch(function (response) {
+      utils.writeJson(res, response);
+    });
+};
