bb5c2dee405f95a35a14b71f5e409497448b258a
billmills <you@example.com>
2023-08-09 11:21:55 -0400

    generalize timeseries templates

---
+++ nodejs-server/controllers/Timeseries.js
@@ -0,0 +1,34 @@
+'use strict';
+
+var utils = require('../utils/writer.js');
+var Timeseries = require('../service/TimeseriesService');
+
+module.exports.findtimeseries = function findtimeseries (req, res, next, timeseriesName, id, startDate, endDate, polygon, multipolygon, winding, center, radius, compression, mostrecent, data) {
+  Timeseries.findtimeseries(timeseriesName, id, startDate, endDate, polygon, multipolygon, winding, center, radius, compression, mostrecent, data)
+    .then(function (response) {
+      utils.writeJson(res, response);
+    })
+    .catch(function (response) {
+      utils.writeJson(res, response);
+    });
+};
+
+module.exports.findtimeseriesMeta = function findtimeseriesMeta (req, res, next, id) {
+  Timeseries.findtimeseriesMeta(id)
+    .then(function (response) {
+      utils.writeJson(res, response);
+    })
+    .catch(function (response) {
+      utils.writeJson(res, response);
+    });
+};
+
+module.exports.timeseriesVocab = function timeseriesVocab (req, res, next, timeseriesName) {
+  Timeseries.timeseriesVocab(timeseriesName)
+    .then(function (response) {
+      utils.writeJson(res, response);
+    })
+    .catch(function (response) {
+      utils.writeJson(res, response);
+    });
+};
--- nodejs-server/service/TimeseriesService.js
+++ nodejs-server/service/TimeseriesService.js
@@ -2,8 +2,9 @@
 
 
 /**
- * NOAA SST search and filter.
+ * Search and filter for timeseries named in path
  *
+ * timeseriesName String 
  * id String Unique ID to search for. (optional)
  * startDate Date ISO 8601 UTC date-time formatted string indicating the beginning of the time period of interest. (optional)
  * endDate Date ISO 8601 UTC date-time formatted string indicating the end of the time period of interest. (optional)
@@ -12,12 +13,12 @@
  * winding String Enforce ccw winding for polygon and multipolygon (optional)
  * center List center to measure max radius from when defining circular region of interest; must be used in conjunction with query string parameter 'radius'. (optional)
  * radius BigDecimal km from centerpoint when defining circular region of interest; must be used in conjunction with query string parameter 'center'. (optional)
- * mostrecent BigDecimal get back only the n records with the most recent values of timestamp. (optional)
  * compression String Data minification strategy to apply. (optional)
+ * mostrecent BigDecimal get back only the n records with the most recent values of timestamp. (optional)
  * data List Keys of data to include. Return only documents that have all data requested, within the pressure range if specified. Accepts ~ negation to filter out documents including the specified data. Omission of this parameter will result in metadata only responses. (optional)
  * returns List
  **/
-exports.findNOAASST = function(id,startDate,endDate,polygon,multipolygon,winding,center,radius,mostrecent,compression,data) {
+exports.findtimeseries = function(timeseriesName,id,startDate,endDate,polygon,multipolygon,winding,center,radius,compression,mostrecent,data) {
   return new Promise(function(resolve, reject) {
     var examples = {};
     examples['application/json'] = [ {
@@ -49,12 +50,12 @@ exports.findNOAASST = function(id,startDate,endDate,polygon,multipolygon,winding
 
 
 /**
- * NOAA SST metadata search and filter.
+ * Metadata for timeseries by ID
  *
  * id String Unique ID to search for. (optional)
  * returns List
  **/
-exports.findNOAASSTmeta = function(id) {
+exports.findtimeseriesMeta = function(id) {
   return new Promise(function(resolve, reject) {
     var examples = {};
     examples['application/json'] = [ {
@@ -102,12 +103,12 @@ exports.findNOAASSTmeta = function(id) {
 
 
 /**
- * List all possible values for certain NOAA sst query string parameters
+ * List data and lattice for the requested timeseries.
  *
- * parameter String query string parameter to summarize possible values of.
+ * timeseriesName String 
  * returns List
  **/
-exports.noaasstVocab = function(parameter) {
+exports.timeseriesVocab = function(timeseriesName) {
   return new Promise(function(resolve, reject) {
     var examples = {};
     examples['application/json'] = [ "", "" ];
