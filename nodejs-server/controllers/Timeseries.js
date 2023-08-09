7bcd7515d53a336e35e1a4e7d69ffeef030d96eb
billmills <you@example.com>
2023-08-09 11:39:21 -0400

    starting to fill out timeseries logic

--- nodejs-server/controllers/Timeseries.js
+++ nodejs-server/controllers/Timeseries.js
@@ -1,12 +1,18 @@
 'use strict';
 
+const apihits = require('../models/apihits');
 var utils = require('../utils/writer.js');
 var Timeseries = require('../service/TimeseriesService');
+var helpers = require('../helpers/helpers')
 
 module.exports.findtimeseries = function findtimeseries (req, res, next, timeseriesName, id, startDate, endDate, polygon, multipolygon, winding, center, radius, compression, mostrecent, data) {
-  Timeseries.findtimeseries(timeseriesName, id, startDate, endDate, polygon, multipolygon, winding, center, radius, compression, mostrecent, data)
-    .then(function (response) {
-      utils.writeJson(res, response);
+  
+  apihits.apihits.create({metadata: req.openapi.openApiRoute, query: req.query})
+
+  Timeseries.findtimeseries(res, timeseriesName, id, startDate, endDate, polygon, multipolygon, winding, center, radius, compression, mostrecent, data)
+    .then(pipefittings => helpers.data_pipeline.bind(null, res)(pipefittings),
+    function (response) {
+      utils.writeJson(res, response, response.code);
     })
     .catch(function (response) {
       utils.writeJson(res, response);
@@ -14,9 +20,13 @@ module.exports.findtimeseries = function findtimeseries (req, res, next, timeser
 };
 
 module.exports.findtimeseriesMeta = function findtimeseriesMeta (req, res, next, id) {
-  Timeseries.findtimeseriesMeta(id)
-    .then(function (response) {
-      utils.writeJson(res, response);
+  
+  apihits.apihits.create({metadata: req.openapi.openApiRoute, query: req.query})
+
+  Timeseries.findtimeseriesMeta(res, id)
+    .then(pipefittings => helpers.data_pipeline.bind(null, res)(pipefittings),
+    function (response) {
+      utils.writeJson(res, response, response.code);
     })
     .catch(function (response) {
       utils.writeJson(res, response);
@@ -24,9 +34,15 @@ module.exports.findtimeseriesMeta = function findtimeseriesMeta (req, res, next,
 };
 
 module.exports.timeseriesVocab = function timeseriesVocab (req, res, next, timeseriesName) {
+  
+  apihits.apihits.create({metadata: req.openapi.openApiRoute, query: req.query})
+
   Timeseries.timeseriesVocab(timeseriesName)
     .then(function (response) {
       utils.writeJson(res, response);
+    },
+    function (response) {
+      utils.writeJson(res, response, response.code);
     })
     .catch(function (response) {
       utils.writeJson(res, response);
