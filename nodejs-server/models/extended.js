f958bb8be609eb1f01c2c886dcf3e8dc6e2e511f
billmills <you@example.com>
2023-11-02 12:43:04 -0400

    first pass at generic extended object api schema

---
+++ nodejs-server/models/extended.js
@@ -0,0 +1,53 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+var vertex = Schema({
+  type: [Number],
+  required: true,
+  validate: {
+    validator: function(v){
+      return v.length == 2
+    },
+    message: x => '${x} is not a valid coordinate set'
+  }
+})
+
+var geolocation = Schema({
+  type: {
+    type: String,
+    required: true,
+    enum: ['MultiPolygon']
+  },
+  coordinates:{
+    type: [[[vertex]]],
+    required: true
+  }
+})
+
+var sourceinfo = Schema({
+  source: {type: [String], required: true},
+  url: {type: String, required: false},
+})
+
+const extendedSchema = Schema({
+  _id: {type: String, required: true},
+  metadata: {type: [String], required: true},
+  geolocation: {type: geolocation, required: true},
+  true_geolocation: {type: geolocation, required: false},
+  raster: {type: [Schema.Types.Mixed], required: false},
+  basin: {type: [Number], required: true}
+});
+
+const extendedMetaSchema = Schema({
+  _id: {type: String, required: true},
+  data_type: {type: String, required: true},
+  data_info: [{type:[Schema.Types.Mixed], required: false}],
+  date_updated_argovis: {type: Date, required: true},
+  source: {type: sourceinfo, required: true}
+});
+
+module.exports = {}
+module.exports['extendedMeta'] = mongoose.model('extendedMeta', extendedMetaSchema, 'extendedMeta');
+module.exports['ar'] = mongoose.model('ar', extendedSchema, 'ar');
+
+
