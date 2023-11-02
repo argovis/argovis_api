fe3dce457ba77c26d9202ddc086e942c6eb8decb
billmills <you@example.com>
2023-11-02 12:33:53 -0400

    controller boilerplate

--- nodejs-server/controllers/Extended.js
+++ nodejs-server/controllers/Extended.js
@@ -1,12 +1,20 @@
 'use strict';
 
+const apihits = require('../models/apihits');
 var utils = require('../utils/writer.js');
 var Extended = require('../service/ExtendedService');
+var helpers = require('../helpers/helpers')
 
 module.exports.extendedVocab = function extendedVocab (req, res, next, extendedName) {
+
+  apihits.apihits.create({metadata: req.openapi.openApiRoute, query: req.query, product: timeseriesName, isWeb: req.headers.origin === 'https://argovis.colorado.edu'})
+  
   Extended.extendedVocab(extendedName)
     .then(function (response) {
       utils.writeJson(res, response);
+    },
+    function (response) {
+      utils.writeJson(res, response, response.code);
     })
     .catch(function (response) {
       utils.writeJson(res, response);
@@ -14,9 +22,13 @@ module.exports.extendedVocab = function extendedVocab (req, res, next, extendedN
 };
 
 module.exports.findExtended = function findExtended (req, res, next, extendedName, id, startDate, endDate, polygon, multipolygon, winding, center, radius, compression, mostrecent) {
-  Extended.findExtended(extendedName, id, startDate, endDate, polygon, multipolygon, winding, center, radius, compression, mostrecent)
-    .then(function (response) {
-      utils.writeJson(res, response);
+
+  apihits.apihits.create({metadata: req.openapi.openApiRoute, query: req.query, product: extendedName, isWeb: req.headers.origin === 'https://argovis.colorado.edu'})
+
+  Extended.findExtended(res, extendedName, id, startDate, endDate, polygon, multipolygon, winding, center, radius, compression, mostrecent)
+    .then(pipefittings => helpers.data_pipeline.bind(null, res)(pipefittings),
+    function (response) {
+      utils.writeJson(res, response, response.code);
     })
     .catch(function (response) {
       utils.writeJson(res, response);
@@ -24,9 +36,41 @@ module.exports.findExtended = function findExtended (req, res, next, extendedNam
 };
 
 module.exports.findextendedMeta = function findextendedMeta (req, res, next, id) {
-  Extended.findextendedMeta(id)
+
+  apihits.apihits.create({metadata: req.openapi.openApiRoute, query: req.query, isWeb: req.headers.origin === 'https://argovis.colorado.edu'})
+
+  Extended.findextendedMeta(res, id)
+    .then(pipefittings => helpers.data_pipeline.bind(null, res)(pipefittings),
+    function (response) {
+      utils.writeJson(res, response, response.code);
+    })
+    .catch(function (response) {
+      utils.writeJson(res, response);
+    });
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+module.exports.timeseriesVocab = function timeseriesVocab (req, res, next, timeseriesName) {
+  
+  apihits.apihits.create({metadata: req.openapi.openApiRoute, query: req.query, product: timeseriesName, isWeb: req.headers.origin === 'https://argovis.colorado.edu'})
+
+  Timeseries.timeseriesVocab(timeseriesName)
     .then(function (response) {
       utils.writeJson(res, response);
+    },
+    function (response) {
+      utils.writeJson(res, response, response.code);
     })
     .catch(function (response) {
       utils.writeJson(res, response);
