41d94f18a5a4c36394d97bd34be9fe7b292a6fe3
billmills <you@example.com>
2023-11-07 12:11:21 -0500

    geo ar test

--- tests/tests/core/arShapes.tests.js
+++ tests/tests/core/arShapes.tests.js
@@ -22,7 +22,7 @@ $RefParser.dereference(rawspec, (err, schema) => {
         const response = await request.get("/extended/ar?polygon=[[-31.56195028862129,-40.41274267694427],[-30.162402425953275,-48.656846368839986],[-19.85493033767642,-47.494728836010914],[-23.12999463207592,-39.42714908325042],[-31.56195028862129,-40.41274267694427]]").set({'x-argokey': 'developer'});
         expect(response.body).to.be.jsonSchema(schema.paths['/extended/{extendedName}'].get.responses['200'].content['application/json'].schema);
         expect(response.body.length).to.eql(1)
-        expect(response.body[0]['_id']).to.eql('2000.01.01.03.0_6')
+        expect(response.body[0]['_id']).to.eql('2000.01.01.03.0_5')
       });
     });
   }
