e5d5abc720d311b767cfb1677aec709201445abc
billmills <you@example.com>
2023-11-07 12:07:19 -0500

    geo ar test

--- tests/tests/core/arShapes.tests.js
+++ tests/tests/core/arShapes.tests.js
@@ -10,24 +10,19 @@ $RefParser.dereference(rawspec, (err, schema) => {
     console.error(err);
   }
   else {
-    // describe("GET /ar", function () {
-    //   it("returns one atmo river result", async function () {
-    //     const response = await request.get("/ar").set({'x-argokey': 'developer'});
-    //     expect(response.body).to.be.jsonSchema(schema.paths['/ar'].get.responses['200'].content['application/json'].schema);
-    //   });
-    // });
-
-    // describe("GET /ar?date", function () {
-    //   it("returns one atmo river result", async function () {
-    //     const response = await request.get("/ar?date=2010-01-01T00:00:00Z").set({'x-argokey': 'developer'});
-    //     expect(response.body).to.be.jsonSchema(schema.paths['/ar'].get.responses['200'].content['application/json'].schema);
-    //   });
-    // });
-
     describe("GET /extended/ar?_id", function () {
-      it("returns one atmo river result", async function () {
+      it("returns one atmo river result by id", async function () {
         const response = await request.get("/extended/ar?id=2000.01.01.03.0_5").set({'x-argokey': 'developer'});
         expect(response.body).to.be.jsonSchema(schema.paths['/extended/{extendedName}'].get.responses['200'].content['application/json'].schema);
+      });
+    });
+
+    describe("GET /extended/ar?polygon", function () {
+      it("returns one atmo river result by geography", async function () {
+        const response = await request.get("/extended/ar?polygon=[[-31.56195028862129,-40.41274267694427],[-30.162402425953275,-48.656846368839986],[-19.85493033767642,-47.494728836010914],[-23.12999463207592,-39.42714908325042],[-31.56195028862129,-40.41274267694427]]").set({'x-argokey': 'developer'});
+        expect(response.body).to.be.jsonSchema(schema.paths['/extended/{extendedName}'].get.responses['200'].content['application/json'].schema);
+        expect(response.body.length).to.eql(1)
+        expect(response.body[0]['_id']).to.eql('2000.01.01.03.0_6')
       });
     });
   }
