0edce514a68e298857f3d9d5575de43ede518a12
billmills <you@example.com>
2023-11-07 11:28:14 -0500

    testing new ars

--- tests/tests/core/arShapes.tests.js
+++ tests/tests/core/arShapes.tests.js
@@ -10,24 +10,24 @@ $RefParser.dereference(rawspec, (err, schema) => {
     console.error(err);
   }
   else {
-    describe("GET /ar", function () {
-      it("returns one atmo river result", async function () {
-        const response = await request.get("/ar").set({'x-argokey': 'developer'});
-        expect(response.body).to.be.jsonSchema(schema.paths['/ar'].get.responses['200'].content['application/json'].schema);
-      });
-    });
+    // describe("GET /ar", function () {
+    //   it("returns one atmo river result", async function () {
+    //     const response = await request.get("/ar").set({'x-argokey': 'developer'});
+    //     expect(response.body).to.be.jsonSchema(schema.paths['/ar'].get.responses['200'].content['application/json'].schema);
+    //   });
+    // });
 
-    describe("GET /ar?date", function () {
-      it("returns one atmo river result", async function () {
-        const response = await request.get("/ar?date=2010-01-01T00:00:00Z").set({'x-argokey': 'developer'});
-        expect(response.body).to.be.jsonSchema(schema.paths['/ar'].get.responses['200'].content['application/json'].schema);
-      });
-    });
+    // describe("GET /ar?date", function () {
+    //   it("returns one atmo river result", async function () {
+    //     const response = await request.get("/ar?date=2010-01-01T00:00:00Z").set({'x-argokey': 'developer'});
+    //     expect(response.body).to.be.jsonSchema(schema.paths['/ar'].get.responses['200'].content['application/json'].schema);
+    //   });
+    // });
 
     describe("GET /ar?_id", function () {
       it("returns one atmo river result", async function () {
-        const response = await request.get("/ar?_id=3_262992").set({'x-argokey': 'developer'});
-        expect(response.body).to.be.jsonSchema(schema.paths['/ar'].get.responses['200'].content['application/json'].schema);
+        const response = await request.get("/ar?_id=2000.01.01.03.0_5").set({'x-argokey': 'developer'});
+        expect(response.body).to.be.jsonSchema(schema.paths['/extended/{extendedName}'].get.responses['200'].content['application/json'].schema);
       });
     });
   }
