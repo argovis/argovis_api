055ca7e446dc8083669917b0b10592128a0b89c3
billmills <you@example.com>
2023-12-10 14:42:59 -0500

    batch meta for drifters

--- nodejs-server/service/DriftersService.js
+++ nodejs-server/service/DriftersService.js
@@ -89,9 +89,10 @@ exports.drifterMetaSearch = function(id,platform,wmo) {
  * compression String Data minification strategy to apply. (optional)
  * mostrecent BigDecimal get back only the n records with the most recent values of timestamp. (optional)
  * data List Keys of data to include. Return only documents that have all data requested, within the pressure range if specified. Accepts ~ negation to filter out documents including the specified data. Omission of this parameter will result in metadata only responses. (optional)
+ * batchmeta String return the metadata documents corresponding to a temporospatial data search (optional)
  * returns List
  **/
-exports.drifterSearch = function(id,startDate,endDate,polygon,multipolygon,winding,center,radius,metadata,wmo,platform,compression,mostrecent,data) {
+exports.drifterSearch = function(id,startDate,endDate,polygon,multipolygon,winding,center,radius,metadata,wmo,platform,compression,mostrecent,data,batchmeta) {
   return new Promise(function(resolve, reject) {
     var examples = {};
     examples['application/json'] = [ {
