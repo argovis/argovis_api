feda4b9eab1b9c9eee15d124c5625b98079b1580
billmills <you@example.com>
2024-03-12 22:05:17 -0400

    remove box search from extended objects

--- nodejs-server/service/ExtendedService.js
+++ nodejs-server/service/ExtendedService.js
@@ -29,7 +29,6 @@ exports.extendedVocab = function(extendedName) {
  * endDate Date ISO 8601 UTC date-time formatted string indicating the end of the time period of interest. (optional)
  * polygon String array of [lon, lat] vertices describing a polygon bounding the region of interest; final point must match initial point (optional)
  * multipolygon String array of polygon regions; region of interest is taken as the intersection of all listed polygons. (optional)
- * box String lon, lat pairs of the lower left and upper right corners of a box on a mercator projection, packed like [[lower left lon, lower left lat],[upper right lon, upper right lat]] (optional)
  * winding String Enforce ccw winding for polygon and multipolygon (optional)
  * center List center to measure max radius from when defining circular region of interest; must be used in conjunction with query string parameter 'radius'. (optional)
  * radius BigDecimal km from centerpoint when defining circular region of interest; must be used in conjunction with query string parameter 'center'. (optional)
@@ -38,7 +37,7 @@ exports.extendedVocab = function(extendedName) {
  * batchmeta String return the metadata documents corresponding to a temporospatial data search (optional)
  * returns List
  **/
-exports.findExtended = function(extendedName,id,startDate,endDate,polygon,multipolygon,box,winding,center,radius,compression,mostrecent,batchmeta) {
+exports.findExtended = function(extendedName,id,startDate,endDate,polygon,multipolygon,winding,center,radius,compression,mostrecent,batchmeta) {
   return new Promise(function(resolve, reject) {
     var examples = {};
     examples['application/json'] = [ {
