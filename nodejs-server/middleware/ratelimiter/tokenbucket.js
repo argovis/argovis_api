2f16048cc5b9ee2e1d7a1936048c3bd9863021c2
bkatiemills <you@example.com>
2025-06-13 13:43:44 -0400

    relax limits on timeseries requests

--- nodejs-server/middleware/ratelimiter/tokenbucket.js
+++ nodejs-server/middleware/ratelimiter/tokenbucket.js
@@ -37,7 +37,7 @@ module.exports.tokenbucket = function (req, res, next) {
 	let cellprice = 0.0001 // token cost of 1 sq deg day
 	let metaDiscount = 100 // scaledown factor to discount except-data-values request by relative to data requests
 	let maxbulk = 2000000 // maximum allowed size of ndays x area[sq km]/13000sqkm; set to prevent OOM crashes
-	let maxbulk_timeseries = 50 // maximum allowed size of area[sq km]/13000sqkm for timeseries; set to prevent OOM crashes
+	let maxbulk_timeseries = 225 // maximum allowed size of area[sq km]/13000sqkm for timeseries; set to prevent OOM crashes
 	let argokey = 'guest'
 	if(req.headers['x-argokey']){
 		argokey = req.headers['x-argokey']
