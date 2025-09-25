7b91610d7b3c1f8aafdf20932deede411efbf9c0
bkatiemills <you@example.com>
2025-09-24 22:07:13 -0400

    typo

--- nodejs-server/controllers/Cchdo.js
+++ nodejs-server/controllers/Cchdo.js
@@ -13,8 +13,8 @@ module.exports.cchdoVocab = function cchdoVocab (req, res, next, parameter) {
     });
 };
 
-module.exports.findCCHDO = function findCCHDO (req, res, next, id, startDate, endDate, polygon, box, center, radius, metadata, woceline, cchdo_cruise, source, compression, data, presRange, verticalRange, batchmeta) {
-  Cchdo.findCCHDO(id, startDate, endDate, polygon, box, center, radius, metadata, woceline, cchdo_cruise, source, compression, data, presRange, verticalRange, batchmeta)
+module.exports.findCCHDO = function findCCHDO (req, res, next, id, startDate, endDate, polygon, box, center, radius, metadata, woceline, expocode, cchdo_cruise, source, compression, data, presRange, verticalRange, batchmeta) {
+  Cchdo.findCCHDO(id, startDate, endDate, polygon, box, center, radius, metadata, woceline, expocode, cchdo_cruise, source, compression, data, presRange, verticalRange, batchmeta)
     .then(function (response) {
       utils.writeJson(res, response);
     })
@@ -23,8 +23,8 @@ module.exports.findCCHDO = function findCCHDO (req, res, next, id, startDate, en
     });
 };
 
-module.exports.findCCHDOmeta = function findCCHDOmeta (req, res, next, id, woceline, cchdo_cruise) {
-  Cchdo.findCCHDOmeta(id, woceline, cchdo_cruise)
+module.exports.findCCHDOmeta = function findCCHDOmeta (req, res, next, id, woceline, expocode, cchdo_cruise) {
+  Cchdo.findCCHDOmeta(id, woceline, expocode, cchdo_cruise)
     .then(function (response) {
       utils.writeJson(res, response);
     })
